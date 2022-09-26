0b246f4d9630eac122b337f8b685cadc6706f19e
Daniil Burnashov <danil-burnashov@mail.ru>
2022-09-26 16:55:16 +0300

    Исправил ошибки

--- UIKit-HW/UIKit-HW/MathAndGameViewController.swift
+++ UIKit-HW/UIKit-HW/MathAndGameViewController.swift
@@ -6,38 +6,39 @@
 //
 
 import UIKit
+// Класс отвечает за сложение чисел и игру "угадай число"
 
 class MathAndGameViewController: UIViewController {
 
     @IBOutlet weak var labelWithName: UILabel!
     @IBOutlet weak var guessLabel: UILabel!
     @IBOutlet weak var summaLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    @IBAction func nameButton(_ sender: Any) {
+    @IBAction func nameButtonAction(_ sender: Any) {
         labelWithName.text = "Привет, "
         askName(title: "Здравствуйте!", message: "Введите имя", style: .alert)
     }
     // MARK: - Label with name
     func askName(title: String, message: String, style: UIAlertController.Style) {
-        let labelNameAlertController = UIAlertController(title: title, message: message, preferredStyle: style)
-        let presentName = UIAlertAction(title: "Готово", style: .default) { (_) in
-            let texta = labelNameAlertController.textFields?.first
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        let presentNameAlertAction = UIAlertAction(title: "Готово", style: .default) { _ in
+            let texta = alertController.textFields?.first
             self.labelWithName.text! += texta?.text ?? "Имя не получено"
         }
-        labelNameAlertController.addTextField { (_) in
-        }
-        labelNameAlertController.addAction(presentName)
-        self.present(labelNameAlertController, animated: true, completion: nil)
+        alertController.addTextField()
+        alertController.addAction(presentNameAlertAction)
+        self.present(alertController, animated: true, completion: nil)
     }
     // MARK: - Math Game
-    @IBAction func mathButton(_ sender: Any) {
-        math()
+    @IBAction func mathButtonAction(_ sender: Any) {
+        configureMathController()
     }
-    func math() {
+    func configureMathController() {
         let alertControl = UIAlertController(title: "Введите числа", message: nil, preferredStyle: .alert)
-        let alertAction = UIAlertAction(title: "Готово", style: .default ) { (_) in
+        let alertAction = UIAlertAction(title: "Готово", style: .default ) { _ in
             let firstNum = Int(alertControl.textFields?[0].text ?? "")
             let secondNum = Int(alertControl.textFields?[1].text ?? "")
             self.summaLabel.text! = "Сумма чисел = \(String(firstNum! + secondNum!))"
@@ -50,11 +51,11 @@ class MathAndGameViewController: UIViewController {
 
     }
     // MARK: - Guess a number game
-    @IBAction func guessGameButton(_ sender: Any) {
-        guessANumber()
+    @IBAction func guessGameButtonAction(_ sender: Any) {
+        configureGuessNumberAlertController()
     }
 
-func guessANumber() {
+func configureGuessNumberAlertController() {
     let guessNumberAlertController = UIAlertController(title: "Угадай число!",
                                                      message: "Введи число от 1 до 10", preferredStyle: .alert)
     let guessNumberAlertAction = UIAlertAction(title: "Угадать!", style: .default) { (_) in
@@ -68,6 +69,6 @@ func guessANumber() {
     }
     guessNumberAlertController.addTextField(configurationHandler: nil)
     guessNumberAlertController.addAction(guessNumberAlertAction)
-    self.present(guessNumberAlertController, animated: true, completion: nil)
+    present(guessNumberAlertController, animated: true, completion: nil)
 }
 }
--- UIKit-HW/UIKit-HW/ReturnHelloViewController.swift
+++ UIKit-HW/UIKit-HW/ReturnHelloViewController.swift
@@ -7,33 +7,38 @@
 
 import UIKit
 
+/*Нужно ввести "leohl" в Алерт нажимаете "ок" отдаете
+ контролеру а контролер отдаст это в модель которая
+ из буквосочетания "leohl" вернет слово "hello"
+ контролеру а контролер отдаст это лейблу и отобразит
+ это на экране*/
+
 class ReturnHelloViewController: UIViewController {
 
     @IBOutlet weak var helloLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    @IBAction func helloButton(_ sender: Any) {
-        self.sayHello(title: "Введите слово", message: "leohl", style: .alert)
+    @IBAction func helloButtonAction(_ sender: Any) {
+        self.configuresayHelloAlertController(title: "Введите слово", message: "leohl", style: .alert)
     }
-    func sayHello(title: String, message: String, style: UIAlertController.Style) {
+    func configuresayHelloAlertController(title: String, message: String, style: UIAlertController.Style) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
-        let alertAction = UIAlertAction(title: "Let's try", style: .default) { (_) in
+        let alertAction = UIAlertAction(title: "Let's try", style: .default) { _ in
             let hello = Hello()
             self.helloLabel.text = hello.newWord(type: alertController.textFields?.first?.text ?? "")
         }
         alertController.addTextField(configurationHandler: nil)
         alertController.addAction(alertAction)
-        self.present(alertController, animated: true, completion: nil)
+        present(alertController, animated: true, completion: nil)
     }
 }
-
+// Возвращает преобразованный hello 
 struct Hello {
     func newWord(type: String) -> String {
-        if type == "leohl" {
+        guard type != "leohl" else {
             return "hello"
-        } else {
-            return "Неверное кодовое слово:)"
         }
-    }
+        return "Неверное кодовое слово:)"
+   }
 }
