d1816f06af303d3587f7f25d16dba0beb21ee2f0
Daniil Burnashov <danil-burnashov@mail.ru>
2022-09-26 07:40:44 +0300

    no message

--- UIKit-HW/UIKit-HWUITests/UIKit_HWUITests.swift
+++ UIKit-HW/UIKit-HWUITests/UIKit_HWUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class UIKit_HWUITests: XCTestCase {
+class UIKitHWUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
