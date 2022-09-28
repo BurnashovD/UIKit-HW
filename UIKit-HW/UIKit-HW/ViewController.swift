170ef12e2cb5b57d52e238f0637c038b4f3fdfff
Daniil Burnashov <danil-burnashov@mail.ru>
2022-09-28 03:03:35 +0300

    no message

---
+++ UIKit-HW/UIKit-HW/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CodeFromLessonViewController.swift
+//  UIKit-HW
+//
+//  Created by Daniil on 28.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
